1046c20ecfd0e620431f14dad99132301d5edcbe
miki <445027453@qq.com>
2022-07-04 16:44:57 +0800

    style:新增 md

--- testPromise.js
+++ testPromise.js
@@ -6,7 +6,7 @@
 // 当 executor 报错的时候，会走到 reject
 // 
 
-const Promise = require('../Promisee')
+// const Promise = require('../Promisee')
 const p1 = new Promise(( resolve,reject ) => {
   // reject ( 12 )
   setTimeout(()=>{
@@ -24,4 +24,25 @@ p1.then( data =>{
   console.log( data,'data' )
 },err => {
   console.log( err,"err" )
+})
+// afternoon
+// then 链条
+// 如果 then 里面是一个 Promise 的话
+  // 如果 是 resolve 下一个 then 里面的 成功
+  // 如果是 reject 的话，下一个 走的是 then 里面的 失败
+// 如果默认不写 then 的成功或者失败 函数
+// 会直接传递给 下一个 then 的 失败或者成功
+const  p2 = new Promise((resolve,reject) => {
+  resolve()
+})
+p2.then(res=>{
+  return Promise.reject(1)
+}, err => {
+  console.log(err, 'err ')
+}).then( res => {
+
+}, err => {
+  console.log(err, 'err then')
+}).catch( err => {
+  console.log(err, 'err catch')
 })
